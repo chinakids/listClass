9cfc7228c117d6c2ac0ae39bd27db8516c1eaab3
chinakids <494063570@qq.com>
2015-08-31 12:30:04 +0800

    格式化

--- 队列/fifo.js
+++ 队列/fifo.js
@@ -3,49 +3,49 @@
  * by   : chinakids
  */
 function Queue(){
-	this.dataStore = [];
-	this.enqueue = enqueue;
-	this.dequeue = dequeue;
-	this.front = front;
-	this.back = back;
-	this.toString = toString;
-	this.empty = empty;
+  this.dataStore = [];
+  this.enqueue = enqueue;
+  this.dequeue = dequeue;
+  this.front = front;
+  this.back = back;
+  this.toString = toString;
+  this.empty = empty;
 }
 function enqueue(element){
-	this.dataStore.push(element);
+  this.dataStore.push(element);
 }
 function dequeue(element){
-	this.dataStore.shift();
+  this.dataStore.shift();
 }
 /*
  * @ 构造优先队列(code为优先级)
  * function dequeue(){
- * 		var priority = this.dataStore.length[0].code;
- * 		for(var i=1;i<this.dataStore.length;++i){
- * 			if(this.dataStore[i].code < priority){
- * 				priority = i;
- * 			}
- * 		}
- * 		return this.dataStore.splice(priority,1);
+ *     var priority = this.dataStore.length[0].code;
+ *     for(var i=1;i<this.dataStore.length;++i){
+ *       if(this.dataStore[i].code < priority){
+ *         priority = i;
+ *       }
+ *     }
+ *     return this.dataStore.splice(priority,1);
  * }
  */
 function front(){
-	return this.dataStore[0];
+  return this.dataStore[0];
 }
 function back(){
-	return this.dataStore[this.dataStore.length];
+  return this.dataStore[this.dataStore.length];
 }
 function toString(){
-	var retStr = '';
-	for (var i = 0;i<this.dataStore.length;++i) {
-		retStr +=this.dataStore[i] + '\n';
-	};
-	return retStr;
+  var retStr = '';
+  for (var i = 0;i<this.dataStore.length;++i) {
+    retStr +=this.dataStore[i] + '\n';
+  };
+  return retStr;
 }
 function empty(){
-	if(this.dataStore.length == 0){
-		return true;
-	}else{
-		return false;
-	}
+  if(this.dataStore.length == 0){
+    return true;
+  }else{
+    return false;
+  }
 }
